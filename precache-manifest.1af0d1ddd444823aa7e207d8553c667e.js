81800e15a203a8c01d9043775aaf8bbd0d4c84cd
Sajjad21990 <sajjadhaider21990@gmail.com>
2020-04-07 12:16:44 +0530

    Updates

---
+++ precache-manifest.1af0d1ddd444823aa7e207d8553c667e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19ed6848b2c6445679e6d1b84ff7c002",
+    "url": "/Covid-19-Live-Tracker-App-using-Reactjs/index.html"
+  },
+  {
+    "revision": "cf1fc6275d6a049bcc2f",
+    "url": "/Covid-19-Live-Tracker-App-using-Reactjs/static/css/main.d4d4230d.chunk.css"
+  },
+  {
+    "revision": "b07a9448d5f4fb50da5c",
+    "url": "/Covid-19-Live-Tracker-App-using-Reactjs/static/js/2.5c3cc670.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-19-Live-Tracker-App-using-Reactjs/static/js/2.5c3cc670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf1fc6275d6a049bcc2f",
+    "url": "/Covid-19-Live-Tracker-App-using-Reactjs/static/js/main.3a7c157a.chunk.js"
+  },
+  {
+    "revision": "4733b1ed1f10e0650307",
+    "url": "/Covid-19-Live-Tracker-App-using-Reactjs/static/js/runtime-main.a3359c32.js"
+  },
+  {
+    "revision": "34967c3bdacffdb6bb6882b73d62c3eb",
+    "url": "/Covid-19-Live-Tracker-App-using-Reactjs/static/media/coronavirus.34967c3b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Covid-19-Live-Tracker-App-using-Reactjs/precache-manifest.aa0ae437ad01b256e658cf60cd2ef91b.js"
+  "/Covid-19-Live-Tracker-App-using-Reactjs/precache-manifest.1af0d1ddd444823aa7e207d8553c667e.js"
 );
 
 self.addEventListener('message', (event) => {
