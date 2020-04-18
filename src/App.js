2a48b63eae711a31fc3025b3941261711095bfa9
Sajjad21990 <sajjadhaider21990@gmail.com>
2020-04-18 16:50:51 +0530

    updated api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Layout1 from "./Layout/Layout1";
 import Layout2 from "./Layout/Layout2";
 
 class App extends React.Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Layout1 />
-          </Route>
-        </Switch>
-      </Router>
+      // <Router>
+      //   <Switch>
+      //     <Route path="/">
+      //       <Layout1 />
+      //     </Route>
+      //   </Switch>
+      // </Router>
+      <Layout1 />
     );
   }
 }
